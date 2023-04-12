53326372d0c9905343bde51a3263c6f8f63a0fa4
unknown <fitrahulanwar95@gmail.com>
2023-04-12 14:53:53 +0700

    laravel crud8

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,41 @@
+@extends('users.layout')
+  
+@section('content')
+   
+<div class="container mt-5">
+   
+    <div class="row justify-content-center align-items-center">
+        <div class="card" style="width: 24rem;">
+            <div class="card-header">
+            Edit User
+            </div>
+            <div class="card-body">
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form method="post" action="{{ route('users.update', $user->id) }}" id="myForm">
+            @csrf
+            @method('PUT')
+                <div class="form-group">
+                    <label for="name">Name</label>                    
+                    <input type="text" name="name" class="form-control" id="name" value="{{ $user->name }}" aria-describedby="name" >                
+                </div>
+                <div class="form-group">
+                    <label for="slug">Slug</label>                    
+                    <input type="text" name="slug" class="form-control" id="slug" value="{{ $user->slug }}" aria-describedby="slug" >                
+                </div>
+                
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
